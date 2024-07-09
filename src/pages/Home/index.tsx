0be90a32e46b370ebc0b1e0fbd1ada1e96305352
pouriya-sedaghat <pouriyac12@gmail.com>
2024-07-09 15:19:19 +0330

    Add Icon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,7 +54,7 @@ function Home() {
       <Row>
         <Col xs={12} className="mb-2">
           <div className="d-flex gap-1 border border-secondary rounded px-3 py-2 mb-2">
-            <i className="bi bi-bag-fill align-self-baseline"></i>
+            <i className="bi bi-bag-fill"></i>
             <input
               type="search"
               value={search}
